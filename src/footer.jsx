9e3c08a909434239f3a2e21a1ac1bab2c66411e7
Leo <leandrohinsua@gmail.com>
2026-05-14 08:08:36 -0300

    Improved footer and kanji learn.

--- src/footer.jsx
+++ src/footer.jsx
@@ -16,27 +16,31 @@ export default function Footer() {
     }, []);
 
     return(
-        <footer>
-            {isHovered ? (
-                <>
-                    <div className="speech-bubble">
-                        <img  
-                        alt="speech-bubble"
-                        height="200px"
-                        width="150px"
-                        src="https://i.imgur.com/fIKLUn8.png" />
-                    </div>
-                    <img className='kanna-img'
-                    onMouseLeave={() => setIsHovered(false)}
-                    src="https://i.imgur.com/4xRvCTu.png" height="150px" width="100px" alt="kanna" />
-                </>
-            ) : (
-                <img className='kanna-img'
-                onMouseOver={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                src="https://i.imgur.com/xsnkDVg.png" height="150px" width="100px" alt="kanna"/>
+        <footer
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            >
+            {isHovered && (
+                <div className="speech-bubble">
+                <img
+                    alt="speech-bubble"
+                    height="200"
+                    width="150"
+                    src="https://i.imgur.com/fIKLUn8.png"
+                />
+                </div>
             )}
-            
-        </footer>
+
+            <img
+                className="kanna-img"
+                src={isHovered
+                    ? "https://i.imgur.com/4xRvCTu.png"
+                    : "https://i.imgur.com/xsnkDVg.png"
+                    }
+                height="150"
+                width="100"
+                alt="kanna"
+            />
+            </footer>
     )
 }
